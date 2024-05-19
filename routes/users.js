fa17879ad25f47cdda97304edf4f56c5c0aee09c
Chinedu Okorie <okoriechinedusunday@gmail.com>
2024-05-19 09:17:58 +0100

    Fix: fix typo

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { getUser, getAUser, createUser, login } = require("../controllers/user");
+const {
+  getUsers,
+  getAUser,
+  createUser,
+  login,
+} = require("../controllers/user");
 
 const userRouter = express.Router();
 
-userRouter.get("/", getUser);
+userRouter.get("/", getUsers);
 userRouter.get("/:id", getAUser);
 userRouter.post("/", createUser);
 userRouter.post("/login", login);
